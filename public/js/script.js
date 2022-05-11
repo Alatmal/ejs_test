096a77c5474a2e0a421b03bd8034bf455a30f927
Alcides <alatmal@gmail.com>
2022-05-11 11:09:05 -0500

    prevent default action

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,25 @@
 let itemsValue= document.querySelector('#num-items');
 let itemsContainer= document.querySelector('.item-container')
 
-itemsValue.addEventListener('change', (event)=> {
-  itemsContainer.innerHTML='';
-    let numofItems= parseInt(event.target.value)
-    if(numofItems>0){
-      let flg= 1;
+
+itemsValue.addEventListener('keypress', function (event) {
+  if (event.keyCode === 13 || e.which === 13) {
       
-
-      for(let i=0; i < numofItems; i++) { 
-        let itm= document.createElement('div');
-        itm.classList.add('item');
-        itm.innerHTML= `<div class="round-number">${flg}</div></div>`
-        itemsContainer.appendChild(itm)
-        flg= flg+1;
+      itemsContainer.innerHTML='';
+      let numofItems= parseInt(event.target.value)
+      if(numofItems>0){
+        let flg= 1;
+        
+  
+        for(let i=0; i < numofItems; i++) { 
+          let itm= document.createElement('div');
+          itm.classList.add('item');
+          itm.innerHTML= `<div class="round-number">${flg}</div></div>`
+          itemsContainer.appendChild(itm)
+          flg= flg+1;
+        }
       }
-    }
-    
-})
-
+      event.preventDefault();
+  }
+  
+});
